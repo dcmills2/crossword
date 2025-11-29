db9ac1a2bc5aee2269fb9ec85eda0b13998dc05a
Daniel Mills <dcmills97@gmail.com>
2025-11-29 13:37:44 -0500

    desktop vs mobile

--- all_encrypt_files.sh
+++ all_encrypt_files.sh
@@ -3,7 +3,7 @@ if [ "$#" -ne 1 ]; then
     echo "Usage: $0 <encryption_key>"
     exit 1
 fi
-for f in `ls unencrypted`; do
+for f in `cat unencrypted/encrypt_list.txt`; do
     ./do_encrypt_file.py $1 unencrypted/$f
+    mv unencrypted/$f.encrypted encrypted_html/
 done
-mv unencrypted/*.encrypted encrypted_html/
