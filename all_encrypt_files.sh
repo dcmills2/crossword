874124fc3c19c79a570b92c822875db7dd1f392f
Daniel Mills <dcmills97@gmail.com>
2025-12-23 20:26:22 -0500

    update index.html to handle refactor. encrypt additional files

--- all_encrypt_files.sh
+++ all_encrypt_files.sh
@@ -4,6 +4,9 @@ if [ "$#" -ne 1 ]; then
     exit 1
 fi
 for f in `cat unencrypted/encrypt_list.txt`; do
-    ./do_encrypt_file.py $1 unencrypted/$f
-    mv unencrypted/$f.encrypted encrypted_html/
+    ./do_encrypt_file.py "$1" "unencrypted/$f"
+    dest=${f#*/} # Remove the first directory component
+    dest_dir=$(dirname "$dest")
+    mkdir -p "encrypted_html/$dest_dir"
+    mv "unencrypted/$f.encrypted" "encrypted_html/${dest}.encrypted"
 done
