a2a4d4c638ae8d6fd1b349f491389e6010406bd7
Daniel Mills <dcmills97@gmail.com>
2025-12-25 17:31:39 -0500

    add update-mini.sh

---
+++ update-mini.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./fetch-and-decrypt-puzzle.sh
+TODAY=$(date +%F)
+git add encrypted/html/mini.json.encrypted encrypted/html/puzzles/$TODAY.json.encrypted
+git commit -m "add $TODAY"
+git push
