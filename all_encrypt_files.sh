4cdf4edc2302a5d9ee33188e17534fb7ff8ca108
Daniel Mills <dcmills97@gmail.com>
2025-12-24 22:05:30 -0500

    Add puzzle chooser

--- all_encrypt_files.sh
+++ all_encrypt_files.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <encryption_key_or_keyfile>"
-    exit 1
-fi
 for f in `cat unencrypted/encrypt_list.txt`; do
-    ./do_encrypt_file.py "$1" "unencrypted/$f"
+    ./do_encrypt_file.py "unencrypted/key.bin" "unencrypted/$f"
     dest=${f#*/} # Remove the first directory component
     dest_dir=$(dirname "$dest")
     mkdir -p "encrypted/html/$dest_dir"
