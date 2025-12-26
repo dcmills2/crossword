6762a7c1d875bf6d4cb4b111b39df0f9ef77c2b4
Daniel Mills <dcmills97@gmail.com>
2025-12-25 19:47:14 -0500

    git pull at begining of update-mini

--- update-mini.sh
+++ update-mini.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+git pull origin master
 bash ./fetch-and-encrypt-puzzle.sh
 TODAY=$(date +%F)
 git add encrypted/html/mini.json.encrypted encrypted/html/puzzles/$TODAY.json.encrypted
 git commit -m "add $TODAY"
-git push
+git push origin master
